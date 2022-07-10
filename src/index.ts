8dd5561b8f518a10d751da12a97549a088fb1546
Chaitanya Kolluru <chaitanya.kolluru@aa.com>
2022-07-09 20:21:35 -0500

    expands the grid with dead cells and passes it back to processGrid

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-import { gridGenerator } from './grid';
+import { gridGenerator } from './gridGenerator';
 import { processGrid } from './processGrid';
 import { Cell } from './Cell';
 
-const awesomeThingsHappen = (grid: Array<Cell>): void => {
-  const gr2 = processGrid(grid);
+const awesomeThingsHappen = (grid: Array<Cell>, n: number): void => {
+  const gr2 = processGrid(grid, n);
   console.log(gr2);
   setTimeout(() => {
-    awesomeThingsHappen(gr2);
+    awesomeThingsHappen(gr2, n);
   }, 10000);
 };
 
-const gr = gridGenerator(5);
+const [gr, n] = gridGenerator(5);
 console.log('initial grid', gr);
-awesomeThingsHappen(gr);
+awesomeThingsHappen(gr, n);
